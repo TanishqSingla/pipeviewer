f756687d13901a84265cc1043a63d94695a21f9c
TanishqSingla <tanishqsingla00@gmail.com>
2021-01-21 22:53:57 +0530

    Separating io tasks to different threads

--- src/read.rs
+++ src/read.rs
@@ -1,8 +1,9 @@
 use crate::CHUNK_SIZE;
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
+use std::sync::{Arc, Mutex};
 
-pub fn read(infile: &str) -> Result<Vec<u8>> {
+pub fn read_loop(infile: &str, quit: Arc<Mutex<bool>>) -> Result<()> {
     let mut reader: Box<dyn Read> = if !infile.is_empty() {
         Box::new(BufReader::new(File::open(infile)?))
     } else {
@@ -10,6 +11,17 @@ pub fn read(infile: &str) -> Result<Vec<u8>> {
     };
 
     let mut buffer = [0; CHUNK_SIZE];
-    let num_read = reader.read(&mut buffer)?;
-    Ok(Vec::from(&buffer[..num_read]))
+
+    loop {
+        let num_read = match reader.read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        Vec::from(&buffer[..num_read]);
+    }
+
+    let mut quit = quit.lock().unwrap();
+    *quit = true;
+    Ok(())
 }
